e56500d5612a6f8251e28fc91abbee163638d422
Bart Stefanski <contact@bstefanski.com>
2026-01-20 02:06:24 +0100

    fix: convert JS arrays to Java arrays for constructor matching

--- loader/src/main/java/com/bmstefanski/hytalejs/JavetFunctionConverter.java
+++ loader/src/main/java/com/bmstefanski/hytalejs/JavetFunctionConverter.java
@@ -3,7 +3,12 @@ package com.bmstefanski.hytalejs;
 import com.caoccao.javet.exceptions.JavetException;
 import com.caoccao.javet.interop.converters.JavetProxyConverter;
 import com.caoccao.javet.values.V8Value;
+import com.caoccao.javet.values.reference.V8ValueArray;
 import com.caoccao.javet.values.reference.V8ValueFunction;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JavetFunctionConverter extends JavetProxyConverter {
   @Override
@@ -12,6 +17,62 @@ public class JavetFunctionConverter extends JavetProxyConverter {
     if (v8Value instanceof V8ValueFunction) {
       return (T) v8Value;
     }
+    if (v8Value instanceof V8ValueArray v8Array) {
+      return (T) convertArrayToJavaArray(v8Array, depth);
+    }
     return super.toObject(v8Value, depth);
+  }
+
+  private Object convertArrayToJavaArray(V8ValueArray v8Array, int depth) throws JavetException {
+    int length = v8Array.getLength();
+    if (length == 0) {
+      return new Object[0];
+    }
+
+    List<Object> elements = new ArrayList<>(length);
+    Class<?> commonType = null;
+
+    for (int i = 0; i < length; i++) {
+      try (V8Value element = v8Array.get(i)) {
+        Object converted = super.toObject(element, depth + 1);
+        elements.add(converted);
+
+        if (converted != null) {
+          Class<?> elementClass = converted.getClass();
+          if (commonType == null) {
+            commonType = elementClass;
+          } else if (!commonType.equals(elementClass)) {
+            commonType = findCommonSuperclass(commonType, elementClass);
+          }
+        }
+      }
+    }
+
+    if (commonType == null || commonType.equals(Object.class)) {
+      return elements;
+    }
+
+    Object array = Array.newInstance(commonType, length);
+    for (int i = 0; i < length; i++) {
+      Array.set(array, i, elements.get(i));
+    }
+    return array;
+  }
+
+  private Class<?> findCommonSuperclass(Class<?> a, Class<?> b) {
+    if (a.isAssignableFrom(b)) {
+      return a;
+    }
+    if (b.isAssignableFrom(a)) {
+      return b;
+    }
+    Class<?> superA = a.getSuperclass();
+    while (superA != null && !superA.equals(Object.class)) {
+      if (superA.isAssignableFrom(b)) {
+        return superA;
+      }
+      superA = superA.getSuperclass();
+    }
+    return Object.class;
   }
 }
